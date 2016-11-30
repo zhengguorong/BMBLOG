ce94fdb1ed2670fe8acdf82763b6e0d1567b0177
郑国榕 <zhengguorong@bluemoon.com.cn>
2016-11-30 12:25:36 +0800

    删除垃圾资源

--- util/tools.js
+++ util/tools.js
@@ -1,14 +1,15 @@
 /**
  * Created by zhengguorong on 2016/11/30.
  */
-var fs = require('fs');
-var mkdirp = require('mkdirp');
+var fs = require('fs')
+var mkdirp = require('mkdirp')
+var path = require('path')
 
 const base64ToImg = (imgData, filePath) => {
     console.log(filePath)
     var base64Data = imgData.replace(/^data:image\/\w+;base64,/, "")
     var dataBuffer = new Buffer(base64Data, 'base64')
-    var fileDir = filePath.substring(0, filePath.lastIndexOf('/'))
+    var fileDir = path.dirname(filePath)
     console.log(fileDir)
     mkdirp(fileDir, (err) => {
         fs.writeFile(filePath, dataBuffer, (err) => {
